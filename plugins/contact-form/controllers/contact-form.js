7629ab0af4a7a8d243d1996c5a4b08047e7bd4a4
Mukut Brahma <mukutb.mac@gmail.com>
2021-07-12 00:44:43 +0530

    created a plugin

---
+++ plugins/contact-form/controllers/contact-form.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * contact-form.js controller
+ *
+ * @description: A set of functions called "actions" of the `contact-form` plugin.
+ */
+
+module.exports = {
+
+  /**
+   * Default action.
+   *
+   * @return {Object}
+   */
+
+  index: async (ctx) => {
+    // Add your own logic here.
+
+    // Send 200 `ok`
+    ctx.send({
+      message: 'ok'
+    });
+  },
+  create: async (ctx) => {
+    ctx.send({
+      message: 'created'
+    })
+  }
+};
